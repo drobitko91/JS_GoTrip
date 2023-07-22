7b8f5afa66f13f9808cd5e9446c51c5b56545006
Anastasia Drobitko <drobitko91@gmail.com>
2023-07-22 22:55:04 +0300

    added JS for buttons in slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        center: true,
-        loop: true,
-        margin: 30,
-        startPosition: 1,
-        items: 3,
-    });
-  });
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true,
+    loop: true,
+    margin: 30,
+    startPosition: 1,
+    items: 3,
+});
 
-  
+$('.slider__btn--prev').click(function() {    
+    owl.trigger('prev.owl.carousel');
+})
+
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
